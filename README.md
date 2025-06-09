83e5d382aa7c4c3ea9652e0de508bcd865604631
KazuakiKuji <kazuaki.kuji@g.dreamcareer.co.jp>
2025-06-09 18:33:25 +0900

    Initial commit from Create Next App

---
+++ README.md
@@ -0,0 +1,4 @@
+# tailwindo-nextjs
+
+## 導入
+[tailwindow](https://tailwindcss.com/docs/installation/using-postcss)
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <h1 className="m-8 border-5 border-solid border-orange-600 bg-yellow-400 p-5 text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
